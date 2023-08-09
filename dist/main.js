d5453995215a05da1ff25bf0ef7d5c62a0e3b74d
jpmunich <parkercoyle8@gmail.com>
2023-08-09 11:21:46 -0400

    Initial Commit

---
+++ dist/main.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const divcontent = document.createElement('h1')
+divcontent.innerHTML = "hello";
+const content = document.getElementById('content');
+
+content.appendChild(divcontent);
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTs7QUFFQSxnQyIsInNvdXJjZXMiOlsid2VicGFjazovL29kaW4tdG8tZG8vLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgZGl2Y29udGVudCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2gxJylcbmRpdmNvbnRlbnQuaW5uZXJIVE1MID0gXCJoZWxsb1wiO1xuY29uc3QgY29udGVudCA9IGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdjb250ZW50Jyk7XG5cbmNvbnRlbnQuYXBwZW5kQ2hpbGQoZGl2Y29udGVudCk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const divcontent = document.createElement('h1')
+divcontent.innerHTML = "hello";
+const content = document.getElementById('content');
+
+content.appendChild(divcontent);
