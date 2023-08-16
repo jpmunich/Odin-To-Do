857857a61e17f7e006c7dc39752cf05669b14437
jpmunich <parkercoyle8@gmail.com>
2023-08-16 08:32:02 -0400

    Initial Commit

---
+++ src/elementCreator.js
@@ -0,0 +1,8 @@
+function createTextElement(textType, text, className, parent) {
+    const element = document.createElement(textType);
+    element.innerText = text;
+    element.classList.add(className);
+    parent.appendChild(element);
+}
+
+export { createTextElement };
