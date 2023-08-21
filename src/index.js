ffdf0c0368c3deec5e21f6b9a5e58a6d0d9cf92e
jpmunich <parkercoyle8@gmail.com>
2023-08-21 14:37:03 -0400

    Built test creating and removing logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import generateHomePage from "./home";
+import { taskManager } from './task.js';
 
 generateHomePage();
-
--- src/task.js
+++ src/task.js
@@ -1,16 +1,30 @@
 let allTasks = [];
 
 const task = (title, description, dueDate, priority, notes, checklist) => {
-    return { title, description, dueDate, priority, notes, checklist };
+    let index = null;
+    return { title, description, dueDate, priority, notes, checklist, index };
 }
 
-export default function addTask() {
-    const newTask = task('Task', 'This is a task', '19 August 2023', 'First', 'Some Notes', 'Everything Checked');
-    allTasks.push(newTask);
-    console.log(allTasks)
-}
+const taskManager = (() => {
+    function addTask() {
+        const newTask = task('Task', 'This is a task', '19 August 2023', 'First', 'Some Notes', 'Everything Checked');
+        allTasks.push(newTask);
+        newTask.index = allTasks.indexOf(allTasks[allTasks.length -  1]);
+    }
+    
+    function removeTask(task) {
+        const removedIndex = task;
+    
+        allTasks.splice(task, 1);
+        allTasks.forEach(todo => {if (todo.index > 0 && todo.index > removedIndex) todo.index -= 1})
+    }
+
+    return { addTask, removeTask };
+})();
+
 
 const project = () => {
     let projectTasks = [];
 }
+export { taskManager };
 
