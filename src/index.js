4dbc0eb9a29ee90cb2e558533dbb5d12f0a9492f
jpmunich <parkercoyle8@gmail.com>
2023-08-30 17:24:04 -0400

    Task + Project Logic

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import generateHomePage from "./home";
+import { taskManager } from "./task.js";
 
 generateHomePage();
-
+taskManager.addTask("Task One");
+taskManager.addTask("Task Two");
+taskManager.addTask("Task Three");
+taskManager.removeTask('Task');
--- src/task.js
+++ src/task.js
@@ -1,14 +1,20 @@
 let allTasks = [];
+let allProjects = [];
 
 const task = (title) => {
     return {title};
+}
+
+const project = (title) => {
+    let tasks = [];
+    return {title, tasks};
 }
 
 const taskManager = (() => {
     const addTask = (title) => {
         const newTask = task(title);
         if (!isInProject(newTask.title)) allTasks.push(newTask);
-        else console.log("ERROR");
+        else console.log('ERROR');
         console.log(allTasks)
     }
 
@@ -27,4 +33,23 @@ const taskManager = (() => {
 
     return { addTask, removeTask };
 })();
-export { taskManager };
+
+const projectManager = (() => {
+    const addProject = (title) => {
+        const newProject = project(title);
+        if (!projectExists(title)) allProjects.push(newProject);
+        else console.log('ERROR');
+    }
+
+    const removeProject = (projectTitle) => {
+        allProjects = allProjects.filter((project) => project.title === projectTitle);
+    }
+    
+    const projectExists = (projectTitle) => {
+        return allProjects.some((project) => project.title === projectTitle);
+    }
+
+    return { addProject, removeProject };
+})();
+
+export { taskManager, projectManager };
